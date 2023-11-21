21b224a5f30d4f05629325a3d7f4474fc10fd812
Luiz Henrique <luizhenriques.assis41@gmail.com>
2023-11-21 17:55:33 -0300

    tipos de dados

---
+++ javascript-para-iniciantes/tipos-de-dados/script.js
@@ -0,0 +1,41 @@
+var nome = 'Ande';
+var sobrenome = 'Rafael';
+var nomeCompleto = nome + ' ' +  sobrenome;
+
+console.log(nomeCompleto);
+
+/*var gols = 1000;
+var frase = 'Romário fez ' + gols + ' gols';
+
+console.log(frase)
+console.log(typeof frase)*/
+
+var ano =' 2018';
+var mes = 8;
+console.log(mes + ' /' + ano )
+
+var melhor = 'teste';
+var frase1 = "Esse é o \"melhor\"  jogo ";
+console.log(frase1);
+
+var gols = 1000;
+var frase2 = `Romário fez ${gols} gols`;
+
+console.log(frase2);
+
+// Declare uma variável contendo uma string
+var letra = '';
+// Declare uma variável contendo um número dentro de uma string
+letra = '2';
+// Declare uma variável com a sua idade
+var minhaIdade = 21
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var meuNome = 'Luiz',
+    sobreNome = 'Henrique';
+var nomeCompleto =`${meuNome} ${sobreNome}`;
+console.log(nomeCompleto)
+// Coloque a seguinte frase em uma variável: It's time
+  var fraSe = " It's time";
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof meuNome);
