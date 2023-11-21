ebd980b9843b985d3c6a942c193febcfd9608cad
Luiz Henrique <luizhenriques.assis41@gmail.com>
2023-11-21 11:26:36 -0300

    inicio aulas

---
+++ javascript-para-iniciantes/variaveis/script.js
@@ -0,0 +1,32 @@
+var nome = 'André';
+var idade = 28;
+var possuiFaculdade = true
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalcomprado =5;
+var totalPreco = totalcomprado * preco;
+console.log('R$',totalPreco)
+
+var sobrenome = 'Rafael', 
+    cidade = 'Rio';
+
+console.log(sobrenome,cidade);
+
+var semDefinir;
+console.log(semDefinir)
+
+//Exercício 
+
+const meuNome = 'Luiz';
+const minhaIdade = 21;
+let comidaFavorita;
+comidaFavorita = 'Hambureguer'
+
+let n1 = 'teste',
+    n2,
+    n3,
+    n5;
+
+console.log(meuNome,minhaIdade,comidaFavorita);
